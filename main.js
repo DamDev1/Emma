01a3716808685b2a4b42d35b57670b0e67976d84
Samuel <fridaydamilare22@gmail.com>
2022-10-03 14:44:35 -0700

    styling and adding

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const tablinks = document.getElementsByClassName("tab-link")
 const tabContent = document.getElementsByClassName("tab-content")
+const container = document.querySelector(".container")
+const ulContainer = document.querySelector(".ulContainer")
+const toggle = document.querySelector(".toggle")
 
 function openTab(tabName){
     for(links of tablinks){
@@ -12,3 +15,17 @@ function openTab(tabName){
     document.getElementById(tabName).classList.add("active-content")
     event.currentTarget.classList.add("active-link")
 }
+
+window.onscroll = function() {myscroll()}
+
+function myscroll() {
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+        container.classList.add("active-container");
+    }else{
+        container.classList.remove("active-container");
+    }
+}
+
+toggle.addEventListener("click", function() {
+    ulContainer.classList.toggle("active-ul")
+})
